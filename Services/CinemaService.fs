656151c81554fcb6139befe95bbd0a8f267182ea
ahmniab <ahmniab11@gmail.com>
2025-12-10 20:18:54 +0200

    feat: implement cinema service with booking and data management functionalities

--- Services/CinemaService.fs
+++ Services/CinemaService.fs
@@ -1,42 +1,11 @@
-namespace CEMSystem.Data
+namespace CEMSystem.Services
 
 open System
 open System.IO
 open System.Text.Json
 
-// Seat status enum
-type SeatStatus =
-    | Available = 0
-    | Booked = 1
+open CEMSystem.Models
 
-// Seat data structure
-[<CLIMutable>]
-type Seat =
-    { Row: int
-      Column: int
-      Status: SeatStatus
-      BookedBy: string option
-      BookingTime: DateTime option }
-
-// Cinema hall configuration
-type CinemaHall =
-    { Width: int // 20 seats
-      Height: int // 11 rows
-      Seats: Seat[,] }
-
-// Booking request
-type BookingRequest =
-    { Row: int
-      Column: int
-      CustomerName: string }
-
-// Booking result
-type BookingResult =
-    | Success of string
-    | SuccessWithTicket of string * TicketInfo // Message and ticket info
-    | SeatAlreadyBooked
-    | InvalidSeat
-    | Error of string
 
 module CinemaService =
     let private dataFilePath = "cinema_bookings.json"
